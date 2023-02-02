348394fc14111ebca5cb3cb783a9dc95b6c4a6cb
yoshiyahoo <josiahtroup@gmail.com>
2023-02-01 21:37:19 -0800

    tried to add some more functionallity, bug with get_element()

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,19 +1,91 @@
-use crate::list::*;
+use std::ptr::NonNull;
 
+use crate::list::*;
 //a struct with <T> works similarly to a template in C++
 pub struct Node<T> {
     //the value at this current node.
     val: T,
 
-    //A box is a smart pointer to the thing inside the box, a node with <T>
+    //NonNull is a smart pointer that's just a raw pointer without a null case
     //Sometimes, we might not be pointing to a node, but pointing to nothing
     //Wrapping an option around the pointer allows us to point to nothing
-    next: Option<Box<Node<T>>>
+    next: Option<NonNull<Node<T>>>
 }
+
+#[derive(Default)]
 pub struct LinkedList<T> {
     //the length of the LinkedList
-    len: u32,
+    len: usize,
 
     //the beginning of the LinkedList. More info about the syntax above.
-    head: Option<Box<Node<T>>>
+    head: Option<NonNull<Node<T>>>
+}
+
+impl<T> Node<T> {
+    fn new(elem: T) -> Self {
+        Self {
+            val: elem,
+            next: None
+        }
+    }
+}
+
+//this is where we implement the List trait functions
+impl<T> List for LinkedList<T> {
+    type Val = T;
+    
+    fn append(&mut self, elem: Self::Val) {
+        unsafe {
+            let mut curr = self.head;
+            while curr != None {
+                curr = (*curr.unwrap().as_ptr()).next;
+            }
+            let next_node = &mut Node::new(elem) as *mut Node<T>;  
+            curr = NonNull::new(next_node);
+            self.len += 1;
+        }
+    }
+
+    fn clear(&self) {
+        todo!();
+    }
+
+    fn get_element(&self, position: usize) -> Result<Self::Val, String> {
+        let mut curr = self.head;
+        
+        if position > self.len || position < 0{
+            Err(String::from("access error: index out of range"))
+        }
+        else {
+            unsafe {
+                for i in 0..position {
+                    curr = (*curr.unwrap().as_ptr()).next;
+                }
+                Ok((*curr.unwrap().as_ptr()).val)
+            }
+        }
+
+    }
+
+    fn get_length(&self) -> usize {
+        //we don't need a semicolon on every line, Rust can interpret this as a u32
+        self.len
+    }
+
+    fn insert(&mut self, position: usize, elem: Self::Val) -> Result<(), String> {
+        todo!();
+    }
+
+    fn is_empty(&self) -> bool {
+        //we don't need a semicolon on every line, Rust can interpret this as a bool
+        self.len == 0
+    }
+
+    fn remove(&mut self, position: usize) -> Result<(), String> {
+        todo!();
+    }
+
+    fn replace(&mut self, position: usize, elem: Self::Val) -> Result<(), String> {
+        todo!();
+    }
 }
--- src/list.rs
+++ src/list.rs
@@ -7,27 +7,38 @@ pub trait List {
     //Rool with it
     type Val;
     
+    //self helps us access a certain instance of something that implements the list trait
+    //we need that instance so we can modify it, like in C++
+
     //add a node as the last item of the list
-    fn append(elem: &Self::Val);
+    fn append(&mut self, elem: Self::Val);
 
     //remove all of the nodes of the list
-    fn clear();
+    fn clear(&self);
 
     //return an item T at a specific position in the list
-    fn get_element(position: usize) -> Option<Self::Val>;
+    //This function should produce an error when our index is out of range
+    //A result enum can wrap our error for us.
+    fn get_element(&self, position: usize) -> Result<Self::Val, String>;
 
-    //return the length of the list
-    fn get_length() -> i32;
+    //return the length of the list as an unsigned number
+    fn get_length(&self) -> usize;
 
-    //inserts the elem at the position specified in the list
-    fn insert(position: usize, elem: &Self::Val) -> Result<(), String>;
+    //inserts the elem at the position specified in the list.
+    //This function should produce an error when we our index is out of range
+    //A result enum can wrap our error for us.
+    fn insert(&mut self, position: usize, elem: Self::Val) -> Result<(), String>;
 
     //checks if the length is 0 or not
-    fn is_empty() -> bool;
+    fn is_empty(&self) -> bool;
 
     //removes the element at the position specified in the list
-    fn remove(position: usize) -> Result<(), String>;
+    //This function should produce an error when our index is out of range
+    //A result enum can wrap our error for us.
+    fn remove(&mut self, position: usize) -> Result<(), String>;
 
     //replaces the elem in the list at the position specified
-    fn replace(position: usize, elem: &Self::Val) -> Result<(), String>;
+    //This function should produce an error when we our index is out of range
+    //A result enum can wrap our error for us.
+    fn replace(&mut self, position: usize, elem: Self::Val) -> Result<(), String>;
 }
