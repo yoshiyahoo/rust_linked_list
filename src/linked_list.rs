017e8421aa6059c1b75768bda5e7885fc746b1fa
yoshiyahoo <josiahtroup@gmail.com>
2023-02-03 22:12:51 -0800

    changed the code alot but finally got append and get_element to work properly

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -1,88 +1,112 @@
-use std::ptr::NonNull;
+use std::ptr::{NonNull, null, null_mut};
+use std::alloc::{alloc, dealloc, Layout};
+
 //a struct with <T> works similarly to a template class in C++
 pub struct Node<T> {
     //the value at this current node.
     val: T,
 
-    //NonNull is a smart pointer that's just a raw pointer without a null case
     //Sometimes, we might not be pointing to a node, but pointing to nothing
     //Wrapping an option around the pointer allows us to point to nothing
-    next: Option<NonNull<Node<T>>>
+    next: *mut Node<T>
 }
 
-#[derive(Default)]
+
 pub struct LinkedList<T> {
     //the length of the LinkedList
     len: usize,
 
     //the beginning of the LinkedList. More info about the syntax above.
-    head: Option<NonNull<Node<T>>>
+    head: *mut Node<T>
 }
 
 impl<T> Node<T> {
     fn new(elem: T) -> Self {
         Self {
             val: elem,
-            next: None
+            next: null_mut() as *mut Node<T>
         }
     }
 }
 
 //this is where we implement the List trait functions
 impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        Self {
+            len: 0,
+            head: null_mut() as *mut Node<T>,
+        }
+    }
     
-    fn append(&mut self, elem: T) {
+    pub fn append(&mut self, elem: T) {
+        let mut ptrs = vec![];
         unsafe {
             let mut curr = self.head;
-            while curr != None {
-                curr = (*curr.unwrap().as_ptr()).next;
-            }
-            let next_node = &mut Node::new(elem);  
-            curr = NonNull::new(next_node);
-            self.len += 1;
-        }
-    }
-
-    fn clear(&self) {
-        todo!();
-    }
-
-    fn get_element(&self, position: usize) -> Result<&T, String> {
-        let mut curr = self.head;
-        
-        if position > self.len {
-            Err(String::from("access error: index out of range"))
-        }
-        else {
-            unsafe {
-                for _i in 0..position {
-                    curr = (*curr.unwrap().as_ptr()).next;
+            loop {
+                if curr != null_mut() {
+                    ptrs.push(curr);
+                    curr = (*curr).next;
                 }
-                Ok(&(*curr.unwrap().as_ptr()).val)
-            }
-        }
+                else {
+                    let layout = Layout::new::<Node<T>>();
+                    let ptr = alloc(layout);
+                    *(ptr as *mut Node<T>) = Node::new(elem);
+                    curr = ptr as *mut Node<T>;
+                    ptrs.push(curr);
 
+                    self.len += 1;
+                    break;
+                }
+            }
+            for i in (1..ptrs.len()).rev() {
+                (*ptrs[i - 1]).next = ptrs[i];
+            }
+            self.head = ptrs[0];
+        }
     }
 
-    fn get_length(&self) -> usize {
+    pub fn clear(&mut self) {
+        self.len = 0;
+        self.head = null_mut();
+    }
+
+    pub fn get_element(&self, position: usize) -> Result<&T, &str> {
+        if position >= self.len {
+            return Err("Position out of range");
+        }
+        unsafe {
+            let mut curr = self.head;
+            for _i in 0..position {
+                if curr != null_mut() {
+                    curr = (*curr).next;
+                }
+                else {
+                    return Err("Position out of range");
+                }
+            }
+            return Ok(&(*curr).val);
+        }
+    }
+
+    pub fn get_length(&self) -> usize {
         //we don't need a semicolon on every line, Rust can interpret this as a usize
         self.len
     }
 
-    fn insert(&mut self, position: usize, elem: T) -> Result<(), String> {
+    pub fn insert(&mut self, position: usize, elem: T) -> Result<(), &str> {
         todo!();
     }
 
-    fn is_empty(&self) -> bool {
+    pub fn is_empty(&self) -> bool {
         //we don't need a semicolon on every line, Rust can interpret this as a bool
         self.len == 0
     }
 
-    fn remove(&mut self, position: usize) -> Result<(), String> {
+    pub fn remove(&mut self, position: usize) -> Result<(), &str> {
         todo!();
     }
 
-    fn replace(&mut self, position: usize, elem: T) -> Result<(), String> {
+    pub fn replace(&mut self, position: usize, elem: T) -> Result<(), &str> {
         todo!();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 mod linked_list;
-
+use linked_list::LinkedList;
 
 fn main() {
-    println!("Hello, world!");
+    let mut linked_list = LinkedList::<i32>::new();
+    linked_list.append(4);
+    linked_list.append(5);
+    linked_list.append(6);
+    println!("{}", linked_list.get_element(0).unwrap());
+    println!("{}", linked_list.get_element(1).unwrap());
+    println!("{}", linked_list.get_element(2).unwrap());
+
+    linked_list.replace(0, 7).unwrap();
+    println!("{}", linked_list.get_element(0).unwrap());
 }
